6bcf7b475cb8feb630465d887ffd3ca635083fa3
Jesse Koldewijn <jesse@iungo.nl>
2022-10-27 11:06:49 +0200

    slight improvement on scss flow (needs more work)

--- apps/next/app/page.tsx
+++ apps/next/app/page.tsx
@@ -1,28 +1,27 @@
 import Image from "next/image"
-import styles from "./page.module.scss"
+import "../styles/home.scss"
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+    <div className="container">
+      <main className="main">
+        <h1 className="title">
           Welcome to <a href="https://nextjs.org">Next.js 13!</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>app/page.tsx</code>
+        <p className="description">
+          Get started by editing <code className="code">app/page.tsx</code>
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://beta.nextjs.org/docs" className={styles.card}>
+        <div className="grid">
+          <a href="https://beta.nextjs.org/docs" className="card">
             <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js 13</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
+            className="card"
           >
             <h2>Examples &rarr;</h2>
             <p>Explore the Next.js 13 playground.</p>
@@ -32,7 +31,7 @@ export default function Home() {
             href="https://vercel.com/templates/next.js/app-directory?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
-            className={styles.card}
+            className="card"
           >
             <h2>Deploy &rarr;</h2>
             <p>Deploy your Next.js site to a public URL with Vercel.</p>
@@ -40,14 +39,14 @@ export default function Home() {
         </div>
       </main>
 
-      <footer className={styles.footer}>
+      <footer className="footer">
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
           Powered by{" "}
-          <span className={styles.logo}>
+          <span className="logo">
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
