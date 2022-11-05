57b7c2ff5e336263e9da9c6371dd593c14836d56
JesseKoldewijn <dev@glitchtech.eu>
2022-11-05 14:48:38 +0100

    config fix on protestprotocol project

--- apps/protestprotocol/next.config.mjs
+++ apps/protestprotocol/next.config.mjs
@@ -1,7 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// @ts-check
+
+/**
+ * Don't be scared of the generics here.
+ * All they do is to give us autocompletion when using this.
+ *
+ * @template {import('next').NextConfig} T
+ * @param {T} config - A generic parameter that flows through to the return type
+ * @constraint {{import('next').NextConfig}}
+ */
+
+function defineNextConfig(config) {
+  return config;
+}
+
+export default defineNextConfig({
   reactStrictMode: true,
   swcMinify: true,
-};
-
-module.exports = nextConfig;
+});
