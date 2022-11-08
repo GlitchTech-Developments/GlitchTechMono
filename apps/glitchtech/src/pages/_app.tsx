fcd79c862937af8d0cf2ee9f1ac222c243e5323a
Jesse Koldewijn <jesse@iungo.nl>
2022-11-08 13:20:15 +0100

    basic setup for glitchtech (seo + theme)

--- apps/glitchtech/src/pages/_app.tsx
+++ apps/glitchtech/src/pages/_app.tsx
@@ -1,8 +1,42 @@
-import "../styles/globals.css";
-import type { AppType } from "next/dist/shared/lib/utils";
+import "../styles/globals.scss";
+// import { useEffect, useRef, useState } from "react";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
+import type { AppProps } from "next/app";
+import { NextSeo } from "next-seo";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <div className="bg-black">
+      <NextSeo
+        titleTemplate="%s | GlitchTech Developments"
+        defaultTitle="Loading..."
+        description="At GlitchTech Developments we work on web, app, software and game projects. We are located in the Netherlands."
+        canonical="https://www.protest-protocol.glitchtech.eu"
+        openGraph={{
+          url: "https://protest-protocol.glitchtech.eu",
+          title: "GlitchTech Developments",
+          description:
+            "At GlitchTech Developments we work on web, app, software and game projects. We are located in the Netherlands.",
+          // images: [
+          //   {
+          //     url: "/GlitchTech.webp",
+          //     width: 800,
+          //     height: 600,
+          //     alt: "At GlitchTech Developments we work on web, app, software and game projects. We are located in the Netherlands.",
+          //     type: "image/jpeg",
+          //   },
+          // ],
+          site_name: "ProtestProtocol",
+        }}
+        twitter={{
+          handle: "@GlitchTechEU",
+          site: "@GlitchTechEU",
+          cardType: "summary_large_image",
+        }}
+      />
+      <Component {...pageProps} />
+    </div>
+  );
+}
 
 export default MyApp;
