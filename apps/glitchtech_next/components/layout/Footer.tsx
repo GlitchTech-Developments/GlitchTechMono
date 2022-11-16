d2b3d7ea9303c2e7eaca84f4003be1e2cd51cb03
JesseKoldewijn <dev@glitchtech.eu>
2022-11-16 23:41:51 +0100

    glitchtech_next (next.js 13 with AppDir - not optimized yet)

---
+++ apps/glitchtech_next/components/layout/Footer.tsx
@@ -0,0 +1,51 @@
+import Link from "next/link"
+import Image from "next/image"
+import { GrGithub /* GrLinkedin */ } from "react-icons/gr"
+// import { SiSteam } from "react-icons/si";
+
+const Footer = () => {
+  const yearStamp = () => {
+    const current = new Date().getFullYear().toString()
+    const origin = "2022"
+
+    if (current == origin) {
+      return "2022"
+    } else {
+      return `2022-${current}`
+    }
+  }
+
+  return (
+    <footer className="footer items-center border-t-[1px] border-glitchtech bg-black p-4 text-neutral-content dark:border-glitchtech">
+      <div className="grid-flow-col items-center">
+        <Link href={"#"} className="hover:text-glitchtech">
+          <Image
+            src={"/images/GlitchTechLogoTransparent.png"}
+            alt="GlitchTech Developments Logo"
+            width={100}
+            height={100}
+            loading="eager"
+            priority={true}
+            className="w-36 select-none"
+          />
+        </Link>
+        <p className="select-none">
+          Copyright © {yearStamp()} | All right reserved
+        </p>
+      </div>
+      <div className="hidden grid-flow-col gap-4 place-self-center justify-self-end md:block">
+        <Link href={"#"} className="hover:text-glitchtech">
+          <GrGithub className="text-2xl" />
+        </Link>
+        {/* <Link href={"#"}>
+          <GrLinkedin className="text-2xl" />
+        </Link> */}
+        {/* <Link href={"#"}>
+          <SiSteam className="text-2xl" />
+        </Link> */}
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ apps/glitchtech_next/components/layout/PageWrapper.tsx
@@ -0,0 +1,24 @@
+"use client"
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import Footer from "./Footer"
+import Banner from "./Partials/Banner"
+import GoToTopBtn from "./Partials/GoToTopBtn"
+
+const PageWrapper = (props: any) => {
+  return (
+    <div>
+      <Banner Title={props.Title} Description={""} />
+      <div className="hero-overlay bg-translucent">
+        <main className="container mx-auto flex min-h-screen flex-col items-center py-12 px-6">
+          {props.children}
+        </main>
+
+        <GoToTopBtn />
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default PageWrapper
